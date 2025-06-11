c919a9362dfa83c4bc046712a703194cba9f0f15
Mahtamun Hoque Fahim <mahtamunhoquefahim@gmail.com>
2025-06-11 12:54:55 +0600

    custom color classes working perfectly now

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'gray-1': '#e9e9e9',
+                'gray-3': '#A0A0A0',
+                'gray-4': '#737373',
+                'gray-5': '#494949',
+                'gray-6': '#212121',
+                'cblue': '#77AAEA',
+                'c-pink': '#E7C1D3',
+                'c-yellow': '#EFDA6E',
+                'c-green': '#A4DAC3',
+                'c-primary': '#ABEF5F',
+                'c-primary-50': '#ABEF5F50',
+                'c-secondary': '#1c1c1c',
+            },
+        },
+    },
+};
